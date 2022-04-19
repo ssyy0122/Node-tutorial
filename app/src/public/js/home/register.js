613aac80eb7337c01319430e6aeef733d1c4cf51
ssyy0122 <z01039118740@gmail.com>
2022-04-19 23:25:15 +0900

    회원가입요청 구현

---
+++ app/src/public/js/home/register.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const id = document.querySelector("#id"),
+  name = document.querySelector("#name"),
+  psword = document.querySelector("#psword"),
+  confirmPsword = document.querySelector("#confirm-psword"),
+  registerBtn = document.querySelector("#button");
+registerBtn.addEventListener("click", register);
+
+function register() {
+  const req = {
+    id: id.value,
+    name: name.value,
+    confirmPsword: confirmPsword.value,
+    psword: psword.value,
+  };
+
+  fetch("/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.success) {
+        location.href = "/";
+      } else {
+        alert(res.msg);
+      }
+    })
+    .catch((err) => {
+      console.error(new Error("회원가입중 에러 발생"));
+    });
+}
